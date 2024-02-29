d63870d2ac850a2fb0f44aad8e417a6183ae406f
Crystal1210 <roche.crystaljane@gmail.com>
2024-02-29 12:14:31 +0800

    getting ready for css

---
+++ src/components/Employee.js
@@ -0,0 +1,13 @@
+function Employee(props) {
+    return ( 
+        <>
+            <h3>Employee {props.name}</h3>
+           
+            {props.role ? (
+            <p class="role">{props.role}</p> 
+            ): (<p class="norole">No role</p>
+            )}
+        </>
+    );
+}    
+export default Employee;
